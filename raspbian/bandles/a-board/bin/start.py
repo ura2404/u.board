c1ad4e86342d537d771953d60a3cee9daf6b245f
ura2404 <ura@itx.ru>
2021-07-22 07:58:46 +0100

    .

--- raspbian/bandles/a-board/bin/start.py
+++ raspbian/bandles/a-board/bin/start.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
-import time
 import os
+import time
 import json
 
 
@@ -11,13 +11,19 @@ data = json.load(open('/home/pi/u.board/config.json'))
 file1 = data["file1"]
 file2 = data["file2"]
 
-def my_callback(channel):
-    # print('You pressed the button')
-    GPIO.remove_event_detect(18)
-    os.system('/home/pi/dbuscontrol.sh pause')
-    os.system('/usr/bin/omxplayer --no-osd /home/pi/u.board/data/'+file2)
-    os.system('/home/pi/dbuscontrol.sh pause')
-    GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback, bouncetime = 200) 
+if os.path.exists(file1) and os.path.exists(file2):
 
-GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback, bouncetime = 200) 
-os.system('/usr/bin/omxplayer --no-osd --loop /home/pi/u.board/data/'+file1)
+    def my_callback(channel):
+        # print('You pressed the button')
+        GPIO.remove_event_detect(18)
+        os.system('/home/pi/u.board/bin/dbuscontrol.sh pause')
+        os.system('/usr/bin/omxplayer --no-osd '+file2)
+        os.system('/home/pi/u.board/bin/dbuscontrol.sh pause')
+        GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback, bouncetime = 200) 
+
+    GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback, bouncetime = 200) 
+    os.system('/usr/bin/omxplayer --no-osd --loop '+file1)
+
+else:
+
+    print('Files not found')
