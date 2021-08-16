1557b093ec959a254b64c94587d671f009d32a40
ura <ura@itx.ru>
2021-08-16 14:29:29 +0300

    .

--- bandles/p-board/raspbian/var/www/u.board/common.php
+++ bandles/p-board/raspbian/var/www/u.board/common.php
@@ -1,5 +1,6 @@
 <?php
-$Config = '/boot/u.board/config.json';
+$Config = realpath(dirname(__FILE__).'/../../../../../../config.json');
+
 if(!file_exists($Config)) die('Config not found');
 
 $Config = json_decode(file_get_contents($Config),true);
